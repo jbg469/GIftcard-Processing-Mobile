f1d846e079a402af3631584d26c71df982466a24
jbg469 <jbg469@nyu.edu>
2022-05-11 01:03:24 -0400

    Pushin p

--- GiftcardSite/app/src/main/java/com/example/giftcardsite/SecondFragment.kt
+++ GiftcardSite/app/src/main/java/com/example/giftcardsite/SecondFragment.kt
@@ -67,8 +67,8 @@ class SecondFragment : Fragment() {
                         Log.d("Register Success", "Register success. Boo.")
                         Log.d("Register Success", "Token:" + loggedInUser?.token.toString())
                         var intent = Intent(Intent.ACTION_VIEW)
-                        intent.type = "text/giftcards_browse"
-                        intent.data = Uri.parse("https://nyuappsec.com/api/index")
+                        //intent.type = "text/giftcards_browse"
+                        //intent.data = Uri.parse("https://nyuappsec.com/api/index")
                         intent.putExtra("User", loggedInUser);
                         startActivity(intent)
                     }
